3778111b0cb0a36c07aa55b93b17d4643c089a13
Jabc <develsoft.jabc@gmail.com>
2025-10-10 11:50:48 -0400

    fix(databaseConfig): fix dabatabase config for production

--- config/database.ts
+++ config/database.ts
@@ -10,17 +10,21 @@ export default ({ env }) => {
         database: env("DATABASE_NAME", "algira"),
         user: env("DATABASE_USERNAME", "algira"),
         password: env("DATABASE_PASSWORD", "algira"),
-        ssl: env.bool("DATABASE_SSL", false) && {
-          key: env("DATABASE_SSL_KEY", undefined),
-          cert: env("DATABASE_SSL_CERT", undefined),
-          ca: env("DATABASE_SSL_CA", undefined),
-          capath: env("DATABASE_SSL_CAPATH", undefined),
-          cipher: env("DATABASE_SSL_CIPHER", undefined),
-          rejectUnauthorized: env.bool(
-            "DATABASE_SSL_REJECT_UNAUTHORIZED",
-            true
-          ),
-        },
+        ssl: env.bool("DATABASE_SSL", false)
+          ? {
+              require: true,
+              rejectUnauthorized: false,
+            }
+          : false,
+
+        // && {
+        //   key: env("DATABASE_SSL_KEY", undefined),
+        //   cert: env("DATABASE_SSL_CERT", undefined),
+        //   ca: env("DATABASE_SSL_CA", undefined),
+        //   capath: env("DATABASE_SSL_CAPATH", undefined),
+        //   cipher: env("DATABASE_SSL_CIPHER", undefined),
+        //   rejectUnauthorized: env.bool(false),
+        // },
         schema: env("DATABASE_SCHEMA", "public"),
       },
       pool: {
