baad2118700b4a7f50b168ec00a85fe36c0b698e
Jabc <develsoft.jabc@gmail.com>
2025-10-10 15:43:47 -0400

    fix(middleware): fix cors to connect to frontend

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,7 +2,14 @@ export default [
   "strapi::logger",
   "strapi::errors",
   "strapi::security",
-  "strapi::cors",
+  // "strapi::cors", para local
+  {
+    name: "strapi::cors",
+    config: {
+      origin: ["https://algira-web.vercel.app/"],
+      credentials: true,
+    },
+  },
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",
